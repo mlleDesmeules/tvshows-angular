c1b4a1ad6ade3313db6a1852c8438129fcd58316
Roxanne Desmeules <roxanne.desmeules@gmail.com>
2019-06-30 17:02:36 -0400

    Update app module & component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { ContentLayoutComponent } from "./layout/content-layout/content-layout.component";
+
+const routes: Routes = [
+	{
+		path        : "",
+		component   : ContentLayoutComponent,
+		loadChildren: () => import("./modules/tvshows/tvshows.module").then(m => m.TvshowsModule),
+	},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { ContentLayoutComponent } from "./layout/content-layout/content-layout.component";
+import { FooterComponent } from "./layout/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		ContentLayoutComponent,
+		FooterComponent,
+	],
+	imports     : [
+		BrowserModule,
+		AppRoutingModule,
+	],
+	providers   : [],
+	bootstrap   : [ AppComponent ],
 })
-export class AppModule { }
+export class AppModule {}
